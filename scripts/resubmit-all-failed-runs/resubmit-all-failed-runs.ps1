f68e67f4ac63b42c56d8a67f4301d4677544d583
Derek Li <derek.li@microsoft.com>
2019-10-01 10:14:44 -0700

    Fix typo

--- scripts/resubmit-all-failed-runs/resubmit-all-failed-runs.ps1
+++ scripts/resubmit-all-failed-runs/resubmit-all-failed-runs.ps1
@@ -1,51 +1,27 @@
-# Max number of pages to follow when retriving runs
-# Each page contains up to 30 runs
-$maxPage = 10
+$startDateTime = Get-Date -Date '11/21/2018 5:00:00 PM'
 
-# Logic Apps to cancel
-$subscriptionName = 'Subscription Name'
-$resourceGroup = 'Resource Group Name'
-$logicAppsName = 'Logic Apps Name'
+$endDateTime = Get-Date -Date '11/21/2018 6:00:00 PM'
 
-
-#####
-
-$subscription = Get-AzSubscription -SubscriptionName $subscriptionName
+$subscription = Get-AzSubscription -SubscriptionName "Name of the subscription"
 
 $cotnext = $subscription | Set-AzContext
 
-# Get token from context for use when making REST call to run API
 $tokens = $cotnext.TokenCache.ReadItems() | where { $_.TenantId -eq $cotnext.Subscription.TenantId } | Sort-Object -Property ExpiresOn -Descending
 
 $token = $tokens[0].AccessToken
 
-$logicApps = Get-AzResource -ResourceType Microsoft.Logic/workflows -ResourceGroupName $resourceGroup -Name $logicAppsName
+$logicApps = Get-AzResource -ResourceType Microsoft.Logic/workflows <# | where { $_.ResourceGroupName -eq 'LoremIpsum'} #>
 
 Foreach ($la in $logicApps) {
-    # Get-AzLogicAppRunHistory does not follow nextLink when retriving runs
-    # https://github.com/Azure/azure-powershell/issues/9141
-    # $runs = Get-AzLogicAppRunHistory -ResourceGroupName $la.ResourceGroupName -Name $la.name | where { $_.Status -eq 'Running' }
+    $runs = Get-AzLogicAppRunHistory -ResourceGroupName $la.ResourceGroupName -Name $la.name | where { $_.Status -eq 'Failed' } | where { $_.StartTime -gt $startDateTime -and $_.StartTime -lt $endDateTime }
 
     $headers = @{
         'Authorization' = 'Bearer ' + $token
     }
 
-    # GET https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Logic/workflows/{workflowName}/runs?api-version=2016-06-01&$top={$top}&$filter={$filter}
-    $nextLink = 'https://management.azure.com/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Logic/workflows/{2}/runs?api-version=2016-06-01&$filter=status eq ''Running''' -f $subscription.Id, $la.ResourceGroupName, $la.Name
-    $runs = @()
-    $page = 0
-
-    Do {
-        $data = Invoke-RestMethod -Method 'GET' -Uri $nextLink -Headers $headers
-
-        $runs += $data.value
-        $nextLink = if ($data.nextLink) { $data.nextLink } else { '' }
-
-        $page++
-    } Until ($nextLink -eq "" -or $page -ge $maxPage)
-
-
     Foreach($run in $runs) {
-        Stop-AzLogicAppRun -ResourceGroupName $la.ResourceGroupName -Name $la.name -RunName $run.name -Force
+        $uri = 'https://management.azure.com/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Logic/workflows/{2}/triggers/{3}/histories/{4}/resubmit?api-version=2016-06-01' -f $subscription.Id, $la.ResourceGroupName, $la.Name, $run.Trigger.Name, $run.Name
+
+        Invoke-RestMethod -Method 'POST' -Uri $uri -Headers $headers
     }
 }
